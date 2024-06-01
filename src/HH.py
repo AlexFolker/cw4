e7c0d87613ae4f30fb0d76bc085d8d72ed5e39b5
Вячеслав <agnostik06@gmail.com>
2024-06-01 22:34:43 +0300

    modify gitignore, add parser and hh classes

---
+++ src/HH.py
@@ -0,0 +1,24 @@
+import requests
+from src.parser import Parser
+
+
+class HH(Parser):
+    """
+    Класс для работы с API HeadHunter
+    Класс Parser является родительским абстрактным классом
+    """
+
+    def __init__(self):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies = []
+
+    def load_vacancies(self, keyword):
+        self.params['text'] = keyword
+        while self.params.get('page') != 2:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
+        return self.vacancies
